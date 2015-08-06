1cd93e94c614697b8f07d1b5ba2b616f420b3929
Nicholas Emanuel <njemanuel01@gmail.com>
2015-08-06 15:05:18 -0500

    Added associations

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,2 +1,4 @@
 class Objective < ActiveRecord::Base
+  belongs_to :rubric
+  has_and_belongs_to_many :users
 end
--- app/models/rubric.rb
+++ app/models/rubric.rb
@@ -1,4 +1,5 @@
 class Rubric < ActiveRecord::Base
   has_and_belongs_to_many :courses
   has_and_belongs_to_many :users
+  has_many :objectives
 end
