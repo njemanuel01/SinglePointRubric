0c144921df3982add6fd21bd0967067d5814c0f0
Nicholas Emanuel <njemanuel01@gmail.com>
2015-08-06 14:37:54 -0500

    Updated associations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  has_many :classes, {where :is_teacher => true}
+  has_many :rubrics, {where :is_teacher => true}
+  has_and_belongs_to_many :classes, {where :is_teacher => false}
+  has_and_belongs_to_many :rubrics, {where :is_teacher => false}
+  has_and_belongs_to_many :objectives, {where :is_teacher => false}
 end
