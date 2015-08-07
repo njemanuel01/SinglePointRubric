c9492c96ad25af3ec740c64d6f1cfb1d3fbb4582
Nicholas Emanuel <njemanuel01@gmail.com>
2015-08-07 09:23:30 -0500

    Added user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
+
   get 'password_resets/create'
 
   get 'password_resets/edit'
 
   get 'password_resets/update'
-
+  
+  resources :password_resets
   resources :users
   resources :users
   resources :objectives
